d3402e561f39127a522e46481a467e3f13adad0c
q517p <56463136+q517p@users.noreply.github.com>
2021-06-13 10:23:13 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  ''pt_key=AAJgwxc6ADC5scTWdJ9a2mS9DJczOICfQwmbWfXkVX1QCklxzXgUmy0Gy18x2TtoVzMC2ew3xYc;pt_pin=wdnXetHBwynisp;,
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
